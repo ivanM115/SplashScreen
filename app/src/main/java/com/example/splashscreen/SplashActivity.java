6d6f30c9bdc0dd94778325bee44d0c691fbbcaf1
ivanM115 <ivanmercadalplatero115@gmail.com>
2026-01-13 20:15:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/splashscreen/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.example.splashscreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Duración del splash (2 segundos)
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }, 2000);
+    }
+}
+
